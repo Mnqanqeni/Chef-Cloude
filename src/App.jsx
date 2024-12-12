7d28f1ef58f6b79491fbf92d58ccd4f2af028b7b
Ndiyakholwa <mnqanqenindiyakholwa206gmail.com>
2024-12-12 20:53:13 +0200

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./componets/Header";
 import Main from "./componets/Main";
-
+import ClaudeRecipe from "./componets/CloudeRecipe";
+import React, { useState } from "react";
 export default function App(){
+  const [ingredient, setIngredient] = useState([])
   return(
     <>
     <Header/>
-    <Main/>
+    <Main ingredient={ingredient} setIngredient={setIngredient}/>
+    <ClaudeRecipe/>
     </>
   )
 }
--- src/componets/Main.jsx
+++ src/componets/Main.jsx
@@ -1,28 +1,41 @@
-import React,{useState} from "react"
 
-export default function Main(){
-    const [ingredient,setIngredient] = useState([])
-    const IngredientHtml=ingredient.map((val,index)=><li key={index}>{val}</li>)
-    console.log(IngredientHtml)
-    const addItem =(event)=>{
+
+export default function Main(props) {
+    const IngredientHtml = props.ingredient.map((val, index) => <li key={index}>{val}</li>)
+    const addItem = (event) => {
         event.preventDefault();
         console.log(event)
-        const formData= new FormData(event.target)
-        setIngredient((prevIngredient)=>[...prevIngredient, formData.get("input")])
+        const formData = new FormData(event.target)
+        props.setIngredient((prevIngredient) => [...prevIngredient, formData.get("input")])
         event.target.reset();
-       
+
     }
     return (
         <main>
-            <form className="add-ingredient-form" onSubmit={addItem}>
-            <input type="text" name="input" placeholder="e.g orange" required></input>
-            <button >Add Ingredient</button>
-            </form>
-            <h1>list of ingredient</h1>
-            <ul>
-            {IngredientHtml}
-            </ul>
-            
+            <div>
+                <form className="add-ingredient-form" onSubmit={addItem}>
+                    <input type="text" name="input" placeholder="e.g orange" required></input>
+                    <button >Add Ingredient</button>
+                </form>
+                {props.ingredient.length ? <section className="main-section">
+                    <div className="content">
+                        <h1>Ingredents on hand:</h1>
+                        <ul className="ingredients-list">
+                            {IngredientHtml}
+                        </ul>
+                    </div>
+                    {props.ingredient.length>=4?<div className="get-recipe-container">
+                        <div>
+                            <h3>Ready for a recipe</h3>
+                            <p>Generate a recipe from your list of ingredients.</p>
+                        </div>
+                        <button>Get a recipe</button>
+                    </div>:null}
+                    
+                </section> : null 
+                }
+
+            </div>
         </main>
     )
 }
